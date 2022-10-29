0fbeea8bf25639be122325922de8f6a203cc44b6
tuan <24a4042615@bav.edu.vn>
2022-10-29 23:02:45 +0700

    update suggest user follow

--- src/sections/sideBar/AccountOffer/SuggestWrapper.js
+++ src/sections/sideBar/AccountOffer/SuggestWrapper.js
@@ -7,23 +7,17 @@ import classNames from 'classnames/bind';
 const cx = classNames.bind(styles);
 
 function SuggestWrapper() {
-    const [followersSuggest, setFollowersSuggestLess] = useState({
-        less: [],
-        more: [],
-    });
+    const [followersSuggest, setFollowersSuggestLess] = useState([]);
     const [countFollowerSuggest, setCountFollowerSuggest] = useState('less');
-    const hanldeChangetypeConutFollowersSuggest = () => {
+    const [moreUsers, setMoreUsers] = useState([]);
+
+    const handleChangeTypeCountUser = () => {
         switch (countFollowerSuggest) {
             case 'less':
-                if (followersSuggest.more.length === 0) {
+                if (followersSuggest.length === 5) {
                     const getFollowerAccount = async () => {
                         const res = await getFollower({ page: '1', per_page: '20' });
-                        setFollowersSuggestLess((prev) => {
-                            return {
-                                ...prev,
-                                more: res.data,
-                            };
-                        });
+                        setMoreUsers(res.data.slice(5));
                     };
                     getFollowerAccount();
                 }
@@ -39,22 +33,21 @@ function SuggestWrapper() {
     useEffect(() => {
         const getFollowerAccount = async () => {
             const res = await getFollower('5');
-            setFollowersSuggestLess((prev) => {
-                return {
-                    ...prev,
-                    less: res.data,
-                };
-            });
+            setFollowersSuggestLess(res.data);
         };
         getFollowerAccount();
     }, []);
     return (
         <div className={cx('wrapper')}>
             <h5 className={cx('title')}>Suggested accounts</h5>
-            {followersSuggest[countFollowerSuggest].map((item) => {
+            {followersSuggest.map((item) => {
                 return <AccountOffer key={item.id} data={item} />;
             })}
-            <span className={cx('btn-more')} onClick={hanldeChangetypeConutFollowersSuggest}>
+            {countFollowerSuggest === 'more' &&
+                moreUsers.map((item) => {
+                    return <AccountOffer key={item.id} data={item} />;
+                })}
+            <span className={cx('btn-more')} onClick={handleChangeTypeCountUser}>
                 {countFollowerSuggest === 'less' ? 'See all' : 'See less'}
             </span>
         </div>
