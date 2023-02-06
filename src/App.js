1b51760ec54d16f40949289181f65f6c7145e93e
tuan <24a4042615@bav.edu.vn>
2023-02-06 23:47:22 +0700

    update react-dom

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
 import React from 'react';
 import { Fragment } from 'react';
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import DefaultLayout from '~/layouts/DefaultLayout';
 import { publicRoutes } from '~/routes';
 
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route>
+            {publicRoutes.map((route, index) => {
+                let Layout = DefaultLayout;
+
+                if (route.layout) {
+                    Layout = route.layout;
+                } else if (route.layout === null) {
+                    Layout = Fragment;
+                }
+
+                const Page = route.component;
+                return (
+                    <Route
+                        key={index}
+                        path={route.path}
+                        element={
+                            <Layout>
+                                <Page />
+                            </Layout>
+                        }
+                    />
+                );
+            })}
+        </Route>,
+    ),
+);
+
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Routes>
-                    {publicRoutes.map((route, index) => {
-                        let Layout = DefaultLayout;
-
-                        if (route.layout) {
-                            Layout = route.layout;
-                        } else if (route.layout === null) {
-                            Layout = Fragment;
-                        }
-
-                        const Page = route.component;
-                        return (
-                            <Route
-                                key={index}
-                                path={route.path}
-                                element={
-                                    <Layout>
-                                        <Page />
-                                    </Layout>
-                                }
-                            />
-                        );
-                    })}
-                </Routes>
-            </div>
-        </Router>
+        <div className="App">
+            <RouterProvider router={router} />
+        </div>
     );
 }
 
